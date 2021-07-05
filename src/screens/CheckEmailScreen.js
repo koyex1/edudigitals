32949f90d589da183e2197d7a91fbcd57a076e34
Olumide Koyenikan <olumide.koyenikan@yahoo.com>
2021-07-05 07:32:33 +0100

    email verificaion disabled

--- src/screens/CheckEmailScreen.js
+++ src/screens/CheckEmailScreen.js
@@ -2,6 +2,7 @@ import { Spin } from 'antd'
 import React from 'react'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
+import { Link } from 'react-router-dom'
 import { hideNavBar } from '../actions/userActions'
 
 function CheckEmailScreen() {
@@ -17,7 +18,7 @@ function CheckEmailScreen() {
 
     return (
         <div  className="checkEmail_welcome">
-            <div className="checkEmail_welcome_text"> <Spin/> Check your email for verification...</div>
+            <div className="checkEmail_welcome_text"> <Spin/>  Temporarily deactivated email verification. You can go right ahead and log in <Link  to="/signin">here</Link> </div>
         </div>
     )
 }
